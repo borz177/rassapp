c16937684c2ec25a9697d46b912549d1c0356f30
borz <borz017795@gmail.com>
2026-04-27 21:11:34 +0300

    update dependencies

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -1,13 +1,21 @@
-import "./SplashScreen.css"
-
-// Если вам нужно убрать сплеш-скрин через время, добавьте таймер в useEffect
-// Если он должен висеть вечно (пока приложение грузится), оставьте так.
-
 export default function SplashScreen() {
   return (
     <div className="splash">
-      {/* Убедитесь, что путь к картинке правильный */}
-      <img src="/splash.png" className="logo" alt="App Logo" />
+      <div className="logo-container">
+        {/* Пульсирующие кольца */}
+        <div className="pulse-ring"></div>
+        <div className="pulse-ring"></div>
+        <div className="pulse-ring"></div>
+
+        {/* Свечение */}
+        <div className="glow"></div>
+
+        {/* Вращающаяся градиентная рамка */}
+        <div className="gradient-border"></div>
+
+        {/* Логотип */}
+        <img src="/splash.png" className="logo" alt="App Logo" />
+      </div>
     </div>
   )
 }
