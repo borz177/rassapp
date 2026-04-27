c8c2e862414c4763a2d037bf193b5e4c1386cc0a
borz <borz017795@gmail.com>
2026-04-27 19:03:09 +0300

    update dependencies

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -1,9 +1,23 @@
 import "./SplashScreen.css"
+import { useEffect, useState } from "react"
 
 export default function SplashScreen() {
+  const [isVisible, setIsVisible] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false)
+      // Здесь переход на главный экран
+    }, 2000) // 2 секунды
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (!isVisible) return null
+
   return (
     <div className="splash">
-      <img src="/splash.png" className="logo" alt="Splash Screen" />
+      <img src="/splash.png" className="logo" alt="Logo" />
     </div>
   )
 }
