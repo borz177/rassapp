3b94c30d97d390584558b39fd93aec0f678e2605
borz <borz017795@gmail.com>
2026-06-23 10:42:22 +0300

    update dependencies

--- App.tsx
+++ App.tsx
@@ -609,7 +609,7 @@ useEffect(() => {
   const now = Date.now();
 
   if (!lastShown || now - Number(lastShown) >= FIVE_HOURS) {
-    setShowTemplateUpdateModal(true);
+    setShowTemplateUpdateModal(false);
     localStorage.setItem(STORAGE_KEY, String(now));
   }
 }, [user, isPublicMode]);
